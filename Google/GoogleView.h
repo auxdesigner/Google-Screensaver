aa9f4b5505a612bb139336cc0a13ad906828cdd8
Liu Liu <lllxjy2009@gmail.com>
2015-07-18 15:48:26 -0700

    initial commit

---
+++ Google/GoogleView.h
@@ -0,0 +1,17 @@
+//
+//  GoogleView.h
+//  Google
+//
+//  Created by liuxliu on 7/18/15.
+//  Copyright (c) 2015 liuxliu. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+#import <WebKit/WebKit.h>
+
+@interface GoogleView : ScreenSaverView
+{
+    WebView *webView;
+}
+
+@end
